01a670d819cd994486b95ae99a441cc02b68a2a3
Heran Gao <herangao6@gmail.com>
2019-10-07 00:03:03 -0400

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -6,8 +6,5 @@ module.exports = {
   db: {
     uri: 'mongodb+srv://Heran:St6007443@gettingstarted-qz8q3.mongodb.net/test?retryWrites=true&w=majority'//place the URI of your mongo database here.
   }, 
-  openCage: {
-    key: '004bf0ce00bb4f6bb9f9030620ca881d' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
-  },
   port: 8080
 };
